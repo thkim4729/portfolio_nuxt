14a0b13de39763e0bc1fdd76f1b11c44df99a07a
THKIM4729\thkim4729 <thkim4729@naver.com>
2024-04-17 16:02:28 +0900

    원복

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,20 +15,11 @@ export default defineNuxtConfig({
         },
       ],
     },
-    // 깃허브 페이지 호스팅시 하단 주석 풀기
-    // baseURL: 'https://github.com/thkim4729/portfolio_nuxt/',
-  },
-  ssr: false,
-  nitro: {
-    preset: 'github-pages',
   },
   css: ['vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css', '@/assets/main.scss'],
   build: {
     transpile: ['vuetify'],
   },
-  // image: {
-  //   dir: 'assets/images',
-  // },
   vite: {
     define: {
       'process.env.DEBUG': false,
