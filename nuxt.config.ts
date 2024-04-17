75b79012c81ab009ac39dd1e7292a97d42e9122c
THKIM4729\thkim4729 <thkim4729@naver.com>
2024-04-17 15:17:06 +0900

    원복

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,6 @@ export default defineNuxtConfig({
       ],
     },
   },
-  ssr: 'false',
   css: ['vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css', '@/assets/main.scss'],
   build: {
     transpile: ['vuetify'],
